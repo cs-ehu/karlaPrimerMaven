4d521d76643191d163f6948b8cea0e3a5be6f274
KarlyHdzC <34174304+KarlyHdzC@users.noreply.github.com>
2019-04-01 11:47:34 -0600

    Add files via upload

---
+++ primerMaven/tutorial_maven_jar/src/main/java/ehu/es/cs19/App.java
@@ -0,0 +1,15 @@
+package ehu.es.cs19;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! c;" );
+        int i = 20; 
+        System.out.println( "Variable i" + i );
+    }
+}
---
+++ primerMaven/tutorial_maven_webapp/target/tutorial_maven_webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! URRA QUEDÓ</h2>
+</body>
+</html>
